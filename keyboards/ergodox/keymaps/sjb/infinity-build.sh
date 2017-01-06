5052dd5bf8271891d52f952f14ae5d07811fd372
SjB <steve@sagacity.ca>
2017-01-06 12:07:43 -0500

    fix: build script will exit on error

--- keyboards/ergodox/keymaps/sjb/infinity-build.sh
+++ keyboards/ergodox/keymaps/sjb/infinity-build.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 make -C ../../../../ ergodox-infinity-sjb
+if [ $? != 0 ]; then
+    exit 1
+fi
 echo "Flashing left hand, Press any key when ready"
 read x
 sudo make -C ../../../../ ergodox-infinity-sjb-dfu-util
+
 make -C ../../../../ ergodox-infinity-sjb MASTER=right
+if [ $? != 0 ]; then
+    exit 1
+fi
 echo "Flashing right hand, Press any key when ready"
 read x
 sudo make -C ../../../../ ergodox-infinity-sjb-dfu-util MASTER=right
