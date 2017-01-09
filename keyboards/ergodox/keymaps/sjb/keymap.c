e598d3ec0ee4684970d6b76e3c194a4e42fc26b6
SjB <steve@sagacity.ca>
2017-01-09 01:33:47 -0500

    added new consumer hid id to keymap
    added Browser, Email and Calculator.

--- keyboards/ergodox/keymaps/sjb/keymap.c
+++ keyboards/ergodox/keymaps/sjb/keymap.c
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |   ESC  |   1  |   2  |   3  |   4  |   5  | MYCM |           | WSCH |   6  |   7  |   8  |   9  |   0  |   -    |
+ * |   ESC  |   1  |   2  |   3  |   4  |   5  | BRWS |           | MYCM |   6  |   7  |   8  |   9  |   0  |   -    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Del    |   Q  |   W  |   E  |   R  |   T  |  [   |           |  ]   |   Y  |   U  |   I  |   O  |   P  |   =    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_ESC,         KC_1,   KC_2,    KC_3,    KC_4,   KC_5,   KC_MYCM,
+        KC_ESC,         KC_1,   KC_2,    KC_3,    KC_4,   KC_5,   KC_BROWSER,
         KC_TAB,         KC_Q,   KC_W,    KC_E,    KC_R,   KC_T,   KC_LBRC,
         KC_LCTRL,       KC_A,   KC_S,    KC_D,    KC_F,   KC_G,
         F(F_LSFT),      KC_Z,   KC_X,    KC_C,    KC_V,   KC_B,   KC_BSLASH,
@@ -67,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                   F(F_LALT),
                                               F(F_CTRL), KC_BSPC, KC_LGUI,
         // right hand
-        KC_WSCH,     KC_6,   KC_7,        KC_8,      KC_9,    KC_0,     KC_MINS,
+        KC_MYCM,     KC_6,   KC_7,        KC_8,      KC_9,    KC_0,     KC_MINS,
         KC_RBRC,     KC_Y,   KC_U,        KC_I,      KC_O,    KC_P,     KC_EQL,
                      KC_H,   KC_J,        KC_K,      KC_L,    KC_SCLN,  KC_QUOT,
         KC_GRAVE,    KC_N,   KC_M,        KC_COMM,   KC_DOT,  KC_SLSH,  F(F_RSFT),
@@ -100,7 +100,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // SYMBOLS
 [SYMB] = KEYMAP(
        // left hand
-       KC_TRNS, KC_F1,     KC_F2,    KC_F3,   KC_F4,   KC_F5,   M(VRSN),
+       KC_TRNS, KC_F1,     KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_MAIL,
        KC_TRNS, KC_TRNS,   KC_UP,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_CAPS, KC_LEFT,   KC_DOWN,  KC_RGHT, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS,   KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
@@ -109,11 +109,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                 KC_MUTE,
                                        KC_TRNS, KC_DELT, KC_TRNS,
        // right hand
-       KC_TRNS, KC_F6,   KC_F7,   KC_F8,     KC_F9,   KC_F10,  KC_F11,
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_INS,    KC_TRNS,  KC_PSCR, KC_F12,
+       KC_CALCULATOR, KC_F6,   KC_F7,   KC_F8,     KC_F9,   KC_F10,  KC_F11,
+       KC_TRNS, KC_TRNS, KC_TRNS, KC_INS,    KC_TRNS, KC_PSCR, KC_F12,
                 KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS,
-                         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                         M(EDITOR), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_PWR,  KC_SLEP,
        KC_SELECT,
        KC_TRNS, KC_TRNS, KC_TRNS
