7e0e7cf856f6469f2d6bb57a9396cf84311714df
SjB <steve@sagacity.ca>
2017-01-09 15:50:18 -0500

    more change to keymap, add new binding

--- keyboards/ergodox/keymaps/sjb/keymap.c
+++ keyboards/ergodox/keymaps/sjb/keymap.c
@@ -10,6 +10,12 @@
 #include "infinity.h"
 #endif
 
+// unsupported hid comsumer id
+#define AL_INTERNET_BROWSER 0x0196
+#define AL_EDITOR 0x185
+
+#define SJB_LCA(kc) (kc | QK_LCTL | QK_LALT)
+
 enum {
   BASE = 0, // default layer
   SYMB,     // symbols
@@ -30,6 +36,8 @@ enum custom_keycodes {
   PLACE_HOLDER = 0, // can always be here
   EPRM,
   VRSN,
+  BROWSER,
+  TSKSWCH,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -58,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_ESC,         KC_1,   KC_2,    KC_3,    KC_4,   KC_5,   KC_BROWSER,
+        KC_ESC,         KC_1,   KC_2,    KC_3,    KC_4,   KC_5,   M(BROWSER),
         KC_TAB,         KC_Q,   KC_W,    KC_E,    KC_R,   KC_T,   KC_LBRC,
         KC_LCTRL,       KC_A,   KC_S,    KC_D,    KC_F,   KC_G,
         F(F_LSFT),      KC_Z,   KC_X,    KC_C,    KC_V,   KC_B,   KC_BSLASH,
@@ -71,7 +79,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_RBRC,     KC_Y,   KC_U,        KC_I,      KC_O,    KC_P,     KC_EQL,
                      KC_H,   KC_J,        KC_K,      KC_L,    KC_SCLN,  KC_QUOT,
         KC_GRAVE,    KC_N,   KC_M,        KC_COMM,   KC_DOT,  KC_SLSH,  F(F_RSFT),
-                             KC_ESC,      F(F_RALT), KC_RGHT, KC_MENU,  F(F_SYMB),
+                             KC_ESC,      F(F_RALT), KC_RGHT, M(TSKSWCH),  F(F_SYMB),
         KC_PGUP,   KC_PGDN,
         F(F_RALT),
         KC_RCTRL,  KC_ENT,   KC_SPC
@@ -79,9 +87,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 1: Symbol Layer
  *
  * ,---------------------------------------------------.           ,--------------------------------------------------.
- * |         |  F1  |  F2  |  F3  |  F4  |  F5  | VRSN |           |      |  F6  |  F7  |  F8  |  F9  |  F10 |   F11  |
+ * |         |  F1  |  F2  |  F3  |  F4  |  F5  | Mail |           | Calc |  F6  |  F7  |  F8  |  F9  |  F10 |   F11  |
  * |---------+------+------+------+------+------+------|           |------+------+------+------+------+------+--------|
- * |         |      |  Up  |      |      |      |      |           |      |      |      | INS  |      | PRSC |   F12  |
+ * |         |      |  Up  |      |      |      | Edit |           | Term |      |      | INS  |      | PRSC |   F12  |
  * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | CAPSLCK | Left | Down | RGHT |      |      |------|           |------|      |      |      |      |      |        |
  * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -101,22 +109,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [SYMB] = KEYMAP(
        // left hand
        KC_TRNS, KC_F1,     KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_MAIL,
-       KC_TRNS, KC_TRNS,   KC_UP,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_TRNS, KC_TRNS,   KC_UP,    KC_TRNS, KC_TRNS, KC_TRNS, KC_WWW_SEARCH,
        KC_CAPS, KC_LEFT,   KC_DOWN,  KC_RGHT, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS,   KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS, F(F_MDIA), KC_TRNS, KC_TRNS, KC_TRNS,
-                                       KC_VOLU, KC_VOLD,
-                                                KC_MUTE,
-                                       KC_TRNS, KC_DELT, KC_TRNS,
+       KC_TRNS, F(F_MDIA), KC_TRNS,  KC_TRNS, KC_TRNS,
+                                     KC_VOLU, KC_VOLD,
+                                              KC_MUTE,
+                                     KC_TRNS, KC_DELT, KC_TRNS,
        // right hand
-       KC_CALCULATOR, KC_F6,   KC_F7,   KC_F8,     KC_F9,   KC_F10,  KC_F11,
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_INS,    KC_TRNS, KC_PSCR, KC_F12,
-                KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS,
-                         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_PWR,  KC_SLEP,
+       KC_CALCULATOR, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+       SJB_LCA(KC_T), KC_TRNS, KC_TRNS, KC_INS,  KC_TRNS, KC_PSCR, KC_F12,
+                      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                               KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_PWR,        KC_SLEP,
        KC_SELECT,
-       KC_TRNS, KC_TRNS, KC_TRNS
+       KC_TRNS,       KC_TRNS, KC_TRNS
        ),
 /* Keymap 2: Media and mouse keys
  *
@@ -165,11 +173,17 @@ const uint16_t PROGMEM fn_actions[] = {
    [F_SYMB]  = ACTION_LAYER_TAP_TOGGLE(SYMB)
   ,[F_MDIA]  = ACTION_LAYER_TOGGLE(MDIA)
   ,[F_LSFT]  = ACTION_MODS_ONESHOT(MOD_LSFT)
-  ,[F_RSFT]  = ACTION_MODS_ONESHOT(MOD_RSFT)
+  ,[F_RSFT]  = ACTION_MODS_ONESHOT(MOD_LSFT)
   ,[F_LALT]  = ACTION_MODS_ONESHOT(MOD_LALT)
   ,[F_RALT]  = ACTION_MODS_ONESHOT(MOD_RALT)
   ,[F_CTRL]  = ACTION_MODS_ONESHOT(MOD_LCTL)
 };
+
+void send_raw_consumer_key(uint16_t code)
+{
+  host_consumer_send(code);
+  host_consumer_send(0);
+}
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
 {
@@ -185,6 +199,14 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
         eeconfig_init();
       }
       break;
+    case BROWSER:
+      if (record->event.pressed) { // send AL_INTERNET_BROWSER to OS
+        send_raw_consumer_key(AL_INTERNET_BROWSER);
+      }
+    case TSKSWCH: 
+      if (record->event.pressed) { 
+        SEND_STRING ("Send LGUI and LALT")
+      }
   }
   return MACRO_NONE;
 };
