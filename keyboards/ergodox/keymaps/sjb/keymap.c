f7f3599a69a93a93f0da3de873938e1d3a4fbd5d
SjB <steve@sagacity.ca>
2017-01-09 01:34:54 -0500

    fix: right layer 1 toggle key
    forgot to relable the key when I cant the fn_action index.
    key should work now.

--- keyboards/ergodox/keymaps/sjb/keymap.c
+++ keyboards/ergodox/keymaps/sjb/keymap.c
@@ -71,7 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_RBRC,     KC_Y,   KC_U,        KC_I,      KC_O,    KC_P,     KC_EQL,
                      KC_H,   KC_J,        KC_K,      KC_L,    KC_SCLN,  KC_QUOT,
         KC_GRAVE,    KC_N,   KC_M,        KC_COMM,   KC_DOT,  KC_SLSH,  F(F_RSFT),
-                             KC_ESC,      F(F_RALT), KC_RGHT, KC_MENU,  F(SYMB),
+                             KC_ESC,      F(F_RALT), KC_RGHT, KC_MENU,  F(F_SYMB),
         KC_PGUP,   KC_PGDN,
         F(F_RALT),
         KC_RCTRL,  KC_ENT,   KC_SPC
