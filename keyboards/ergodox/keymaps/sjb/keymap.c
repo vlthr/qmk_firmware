8bff0be9dab875ad1536e561a96f729413757cf9
SjB <steve@sagacity.ca>
2017-01-14 22:19:59 -0500

    Left and Right shift should be the same for OSM to work.
    If both shift are not the same key the ONESHOT will not cancel the
    other out.

--- keyboards/ergodox/keymaps/sjb/keymap.c
+++ keyboards/ergodox/keymaps/sjb/keymap.c
@@ -136,7 +136,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         TD_TGWIN, KC_6,    KC_7,     KC_8,      KC_9,      KC_0,     KC_MINS,
         TD_GRV,   KC_Y,    KC_U,     KC_I,      KC_O,      KC_P,     KC_EQL,
                   KC_H,    KC_J,     KC_K,      KC_L,      KC_SCLN,  KC_QUOT,
-        TD_RBRC,  KC_N,    KC_M,     KC_COMM,   KC_DOT,    KC_SLSH,  KC_RSFT,
+        TD_RBRC,  KC_N,    KC_M,     KC_COMM,   KC_DOT,    KC_SLSH,  F_LSFT,
                            F_NUMPAD, KC_DOWN,   TD_RGHT,   F_RALT,   M(TSKSWCH),
         KC_PGUP,  KC_PGDN,
         F_MEH,
