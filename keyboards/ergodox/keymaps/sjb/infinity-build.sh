254e026108ec95a112ffd155f6ec018caf597253
SjB <steve@sagacity.ca>
2017-01-17 01:43:09 -0500

    change build script to only build oneside.
    building both side always required a full recompile. if I find
    something wrong on the right hand then we can fix it without building
    the left hand.

--- keyboards/ergodox/keymaps/sjb/infinity-build.sh
+++ keyboards/ergodox/keymaps/sjb/infinity-build.sh
@@ -1,16 +1,23 @@
 #!/bin/sh
-make -C ../../../../ ergodox-infinity-sjb
+
+flags=""
+if [ ! -z $1 ]; then
+	flags="MASTER=right"
+fi
+
+make -C ../../../../ ergodox-infinity-sjb $flags
 if [ $? != 0 ]; then
     exit 1
 fi
 echo "Flashing left hand, Press enter key to continue"
 read x
-sudo make -C ../../../../ ergodox-infinity-sjb-dfu-util
+sudo make -C ../../../../ ergodox-infinity-sjb-dfu-util $flags
 
-make -C ../../../../ ergodox-infinity-sjb MASTER=right
-if [ $? != 0 ]; then
-    exit 1
-fi
-echo "Flashing right hand, Press enter key to continue"
-read x
-sudo make -C ../../../../ ergodox-infinity-sjb-dfu-util MASTER=right
+
+#make -C ../../../../ ergodox-infinity-sjb MASTER=right
+#if [ $? != 0 ]; then
+#    exit 1
+#fi
+#echo "Flashing right hand, Press enter key to continue"
+#read x
+#sudo make -C ../../../../ ergodox-infinity-sjb-dfu-util MASTER=right
