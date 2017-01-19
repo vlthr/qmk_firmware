1756936ab709c29c584c9c77b1c7366ac6bd265a
SjB <steve@sagacity.ca>
2017-01-19 01:31:31 -0500

    suppress message: warning: implicit declaration of function
    needed to include the serial_link.h file in matrix.c

--- keyboards/ergodox/infinity/matrix.c
+++ keyboards/ergodox/infinity/matrix.c
@@ -26,7 +26,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "matrix.h"
 #include "serial_link/system/serial_link.h"
 
-
 /*
  * Infinity ErgoDox Pinusage:
  * Column pins are input with internal pull-down. Row pins are output and strobe with high.
