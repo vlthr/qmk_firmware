3a58d3977624864f240514e1bec736aec3c5621f
Valthor Halldorsson <halldorsson.v@gmail.com>
2017-03-26 15:23:12 +0000

    added [] as on kinesis, clarified some comments, added DEL

--- keyboards/ergodox/keymaps/vlthr/keymap.c
+++ keyboards/ergodox/keymaps/vlthr/keymap.c
@@ -137,17 +137,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | TAB    |   Q  |   W  |   E  |   R  |   T  |  \   |           |  `   |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | LCTRL  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '    |
- * |--------+------+------+------+------+------|  [   |           |  ]   |------+------+------+------+------+--------|
+ * |--------+------+------+------+------+------| ~L2  |           |  ]   |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | FNx  | LALT | LEFT |  UP  |  ~L1 |                                       | ~L2  | DOWN | RGHT | RALT | SWCH |
+ *   | FNx  | LALT | LEFT |  UP  |  ~L1 |                                       | RALT  | DOWN |  [  |  ]   | FNx  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | LALT | LGUI |       | PGUP | PGDN |
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      | HYPR |       | MEH  |        |      |
  *                                 | LCTRL| BKSP |------|       |------|  ENTER | [ ]  |
- *                                 |      |      | LGUI |       | Esc  |        |      |
+ *                                 |      |      | DEL  |       | Esc  |        |      |
  *                                 `--------------------'       `----------------------'
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
@@ -157,17 +157,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_EQL,   KC_1,    KC_2,    KC_3,   KC_4,       KC_5,     F_TERM,
         KC_TAB,   KC_Q,    KC_W,    KC_E,   KC_R,       KC_T,     TD_BSLS,
         KC_LCTL,  KC_A,    KC_S,    KC_D,   KC_F,       KC_G,
-        KC_LSFT,  KC_Z,    KC_X,    KC_C,   KC_V,       KC_B,     TD_LBRC,
+        KC_LSFT,  KC_Z,    KC_X,    KC_C,   KC_V,       KC_B,     F_NUMPAD,
         F_FNx,    KC_GRV,  KC_GRV, KC_LGUI,F_MOTION,
                                                         KC_LALT,   KC_LGUI,
                                                                    KC_HYPR,
-                                           KC_LCTL,     KC_BSPC,   KC_LGUI,
+                                           KC_LCTL,     KC_BSPC,   KC_DEL,
         // right hand
         KC_MYCM,  KC_6,    KC_7,     KC_8,      KC_9,      KC_0,     KC_MINS,
         TD_GRV,   KC_Y,    KC_U,     KC_I,      KC_O,      KC_P,     KC_BSLS,
-                  KC_H,    KC_J,     KC_K,      KC_L,      TD_SCLN,  KC_QUOT,
+                  KC_H,    KC_J,     KC_K,      KC_L,      KC_SCLN,  KC_QUOT,
         TD_RBRC,  KC_N,    KC_M,     KC_COMM,   KC_DOT,    KC_SLSH,  KC_RSFT,
-                           F_NUMPAD, LT_DOWN,   LT_UP,     KC_RALT,  F_FNx,
+                           KC_RALT,  LT_DOWN,   KC_LBRC,   KC_RBRC,  F_FNx,
         KC_PGUP,  KC_PGDN,
         F_MEH,
         KC_ESC,   KC_ENT, KC_SPC
